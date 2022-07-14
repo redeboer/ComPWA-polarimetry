3edb538ab34cb859b2cd7d8e13d83e57b4d2ad34
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-07-14 12:07:32 +0200

    run the benchmark

--- julia/scripts/benchmark.jl
+++ julia/scripts/benchmark.jl
@@ -3,15 +3,9 @@ using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 #
-import YAML
-using JSON
-using Plots
-using LaTeXStrings
-import Plots.PlotMeasures.mm
+using YAML
 #
 using Parameters
-using Measurements
-using DataFrames
 #
 using ThreeBodyDecay
 
@@ -19,6 +13,18 @@ using Lc2ppiKModelLHCb
 
 using BenchmarkTools
 using InteractiveUtils
+using ThreadsX
+using LinearAlgebra
+
+
+import Plots.PlotMeasures.mm
+using Plots
+using LaTeXStrings
+
+theme(:wong2, frame=:box, grid=false, minorticks=true,
+    guidefontvalign=:top, guidefonthalign=:right,
+    xlim=(:auto, :auto), ylim=(:auto, :auto),
+    lw=1, lab="", colorbar=false)
 
 #                                  _|            _|
 #  _|_|_|  _|_|      _|_|      _|_|_|    _|_|    _|
@@ -36,12 +42,56 @@ const model0 = LHCbModel(
     particledict=isobarsinput)
 
 const λσs0 = randomPoint(tbs)
+const σs0 = λσs0.σs
+const two_λs = λσs0.two_λs
 
-# check that the type is correctly inferred
-@code_warntype amplitude(λσs0, model0.chains[3])
 
-# run several times, get time
-@btime amplitude.($(Ref(λσs0)), $(model0.chains))
+function intensity(m::LHCbModel, σs)
+    @unpack chains, couplings = m
+    #
+    val = 0.0
+    for two_λ0 in (-1, 1), two_λ1 in (-1, 1)
+        two_λs = (two_λ1, 0, 0, two_λ0)
+        av = amplitude.(chains, Ref(σs), Ref(two_λs))
+        val += abs2(dot(av, couplings))
+    end
+    return val
+end
 
-# full information
-@benchmark amplitude.($(Ref(λσs0)), $(model0.chains))
+intensity(model0, σs0)
+
+
+# @code_warntype amplitude(model0.chains[1], σs0, two_λs)
+
+# intensity(model0, σs0)
+
+
+function benchmarkonset(model, Nev)
+    ms = model.chains[1].tbs.ms
+    data = flatDalitzPlotSample(ms; Nev)
+    @belapsed intensity.(Ref($(model)), $(data))
+end
+
+function benchmarkonset_threads(model, Nev)
+    ms = model.chains[1].tbs.ms
+    data = flatDalitzPlotSample(ms; Nev)
+    @belapsed ThreadsX.map(d -> intensity($(model), d), $(data))
+end
+
+sampleNev = [10, 100, 1000, 10000, 30000]
+tv = benchmarkonset.(Ref(model0), sampleNev)
+tv′ = benchmarkonset_threads.(Ref(model0), sampleNev)
+
+let
+    plot(xlab="Number of events", ylab="Computation time",
+        ylim=(:auto, :auto), xscale=:log10, yscale=:log10)
+    plot!(sampleNev, tv, lab="1 core", m=(:o, 3))
+    plot!(sampleNev, tv′, lab="8 cores", m=(:o, 3))
+end
+
+# benchmarkonset_threads(model0, 100)
+
+
+# benchmarkonset(model0, 100)
+# benchmarkonset(model0, 1000)
+# benchmarkonset(model0, 10000)
